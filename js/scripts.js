c3e4bb07177063ca1a4e5d66907e2f3f53b52de5
[brycen] <[brycenbartolome@gmail.com]>
2020-08-18 10:24:59 -0700

    refactor code to use for
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
   
-  const favoriteIceCream = ["pistacio", "cheesecake", "cherry", "salted caramel"];
+  // let favoriteIceCream = ["pistacio", "cheesecake", "cherry", "salted caramel"];
 
-  favoriteIceCream.forEach(function(iceCream) {
-    let flavorsILike = "I Like ";
-    flavorsILike = flavorsILike.concat(" " + iceCream + " IceCream!");
+  // favoriteIceCream.forEach(function(iceCream) {
+  let flavorsILike = "I Like ";
+  let favoriteIceCream = ["pistacio", "cheesecake", "cherry", "salted caramel"];
+    for (let index = 0; index < favoriteIceCream.length; index +=1) {
+      //flavorsILike = flavorsILike.concat(" " + favoriteIceCream[index] + " icecream!");
+      $("#iceCreams").append("<li>I Like " + favoriteIceCream[index] + "!</li>");
+    }
+    // flavorsILike = flavorsILike.concat(" " + iceCream + " IceCream!");
+    
     console.log(flavorsILike);
   
-  // $("#iceCreams").append(flavorsILike);
-  $("#iceCreams").append("<li>" + flavorsILike + "</li>");
+  // $("#iceCreams").append(flavorsILikee
+  //$("#iceCreams").append("<li>" + flavorsILike + "</li>");
   // alert(flavorsILike);
-  });
-
-  
 });
+
+
